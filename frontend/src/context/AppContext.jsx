5d83a3a1c3601684851c42c842404c5979c4983b
Abhay Pratap Singh <abhay171010@gmail.com>
2025-06-09 09:59:26 +0530

    first

---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,31 @@
+import {  createContext,useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
+
+
+export const AppContext=createContext ()
+
+
+export const AppContextProvider=({children})=>{
+    const navigate=useNavigate();
+    const [user,setUser]=useState(true)
+    const [isSeller,setIsSeller]=useState(false);
+    const [showUserLogin,setShowUserLogin]=useState(false);
+
+    const value = {
+        navigate,user, setUser,setIsSeller,isSeller,showUserLogin,setShowUserLogin
+         
+    }
+    return <AppContext.Provider value={value}>
+            {children}
+
+        </AppContext.Provider>
+}
+
+export const useAppContext =()=>{
+    return useContext(AppContext)
+}
+
+// 24 min
